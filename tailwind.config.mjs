da53411d8d22b5034a4520e1200aff268fc2b3a0
joriman <jamonj@protonmail.com>
2023-12-11 22:12:33 +0200

    feat: section curves svg and begin move to tailwind

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,6 +3,18 @@ export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     extend: {},
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      'white': '#ffffff',
+      'purple': '#a000ff',
+      'midnight': '#121063',
+      'metal': '#565584',
+      'tahiti': '#3ab7bf',
+      'silver': '#ecebff',
+      'bubble-gum': '#ff77e9',
+      'bermuda': '#78dcca',
+    },
   },
   plugins: [],
 };
